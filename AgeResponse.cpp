6aabecd0d07caf601f5611f8cad492df66ee439b
jiwpark00 <jiwpark00@gmail.com>
2021-12-24 00:38:38 -0500

    First C++ file using age response Q

---
+++ AgeResponse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Following along a book tutorial
+
+int main() {
+  int age{}; // age is a variable of an integer type (int)
+  cout << "How old are you?" << endl;
+  cin >> age;
+
+  cout << "wow, we're the same age! I am also " << age << " years old! How about that!" << endl;
+} 

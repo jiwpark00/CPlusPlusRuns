58f3909631faf54e08fa8d6ff864f534d391c7e6
jiwpark00 <jiwpark00@gmail.com>
2021-12-28 21:58:57 -0500

    Update ufo.cpp
    finished the ufo.cpp file

--- ufoExercises/ufo.cpp
+++ ufoExercises/ufo.cpp
@@ -17,14 +17,26 @@ int main() {
 while (answer != codeword && misses < 7) {
     display_misses(misses);
     display_status(incorrect, answer);
-    std::cout << "Please enter your guess: ";
-    std::cin >> letter >> "\n";
+    std::cout << "Please enter your guess: \n";
+    std::cin >> letter;
 
     for (int i = 0; i < codeword.size(); i++) {
-      
+      if (letter == codeword[i]) {
+        answer[i] = letter;
+        guess = true;
+      }
+    }
+    if (guess) {
+    std::cout << "Correct!\n";
     }
 
-    misses++;
+    else {
+      std::cout << "Incorrect! The tractor beam pulls the person in further.\n";
+      incorrect.push_back(letter);
+      misses++;
+    }
+
+    guess = false;
 
   }
 
